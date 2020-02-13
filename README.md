35c2f2da183f7fb2091c05084c2846e3d6cbf1c7
Esken-VIA <isvia18081999@gmail.com>
2020-02-13 17:29:56 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my-app-lab-4-zisagc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/my-app-lab-4-zisagc)
---
+++ src/app/products.ts
@@ -0,0 +1,120 @@
+export const products = [
+  {
+    tag:[1, 2, 3],
+    numeration: '1',
+    name: 'Fucking Phone 11 Pro Max',
+    price: 799,
+    description: 'A large phone with three fucking cameras',
+    rating: 4.5,
+    link: 'https://tmall.ru/item/Smartphone-Apple-iPhone-11-Pro-Max-256GB/4000171918592.html?algo_pvid=e3916b31-f9ff-44c0-af97-901f8c9826b2?spm=a2g0r.search0104.3.1.c7e769c4TcM0M3&algo_expid=e3916b31-f9ff-44c0-af97-901f8c9826b2-0&priceBeautifyAB=0',
+    mainImage: 'https://ae01.alicdn.com/kf/Hfbafaf39c1b2498283452cee4ebf20b9A/Apple-iPhone-11-Pro-Max-512GB.jpg_640x640.jpg',
+    secondImage:'https://ae01.alicdn.com/kf/H36c5acfefc724ebdbf2a9f1e79d55ddbt/Apple-iPhone-11-Pro-Max-512GB.jpg',
+    thirdImage: 'https://ae01.alicdn.com/kf/H495eeff6c7e54a3baaf08216a82c21420/Apple-iPhone-11-Pro-Max-256GB.jpg_50x50.jpg'
+  },
+  {
+    numeration: '2',
+    name: 'Fucking Phone 11',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    rating: 4.5,
+    link: 'https://tmall.ru/item/Smartphone-Apple-iPhone-11-64-GB/4000171735887.html?algo_pvid=bc958ebd-7681-4a71-80d1-c553fc29755b?spm=a2g0r.search0104.3.1.24944780ucu2fn&algo_expid=bc958ebd-7681-4a71-80d1-c553fc29755b-0&priceBeautifyAB=0',
+    mainImage:'https://ae01.alicdn.com/kf/H1ee530d0c837453484351664e59541d2o/Apple-iPhone-11-64GB.jpg_50x50.jpg',
+    secondImage:'https://ae01.alicdn.com/kf/H4294b4ffc5174b6b976b671a3088b5ccm/Apple-iPhone-11-64GB.jpg_50x50.jpg',
+    thirdImage:'https://ae01.alicdn.com/kf/Haa7526a047fc45a7b252b0944160fb5ce/Apple-iPhone-11-64GB.jpg_50x50.jpg'
+  },
+  {
+    numeration: '3',
+    name: 'Fucking Phone XS',
+    price: 299,
+    description: 'smth cool',
+    rating: 4.5,
+    link:'https://tmall.ru/item/IPhone-XS-iPhone-XS-64-GB-smartphone-iOS-12-nano-SIM-eSIM-3G-4G-LTE-LTE/4000275646743.html?algo_pvid=b3111a45-569d-4ed8-ac62-fb648969f354?spm=a2g0r.search0104.3.1.6d25140bcXt1wp&algo_expid=b3111a45-569d-4ed8-ac62-fb648969f354-0&priceBeautifyAB=0',
+    mainImage:'https://ae01.alicdn.com/kf/Hc1b5a6baa52d478b82acd1efde8073b6F/XS-Apple-iPhone-XS-64-iOS-12-nano-SIM-eSIM-3G-4G.jpg_50x50.jpg',
+    secondImage:'https://ae01.alicdn.com/kf/H217d613ec08e4eb4b84ce2e6da7c8c32C/XS-Apple-iPhone-XS-64-iOS-12-nano-SIM-eSIM-3G-4G.jpg_50x50.jpg',
+    thirdImage:'https://ae01.alicdn.com/kf/Hf83a06e92cba462097a5ab9576850c5fX/XS-Apple-iPhone-XS-64-iOS-12-nano-SIM-eSIM-3G-4G.jpg_50x50.jpg'
+  },
+  {
+    numeration: '4',
+    name: 'Fucking Phone X',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    rating: 4.5,
+    link:'',
+    mainImage:'',
+    secondImage:'',
+    thirdImage:''
+  },
+  {
+    numeration: '5',
+    name: 'Fucking Phone 8',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    rating: 4.5,
+    link:'',
+    mainImage:'',
+    secondImage:'',
+    thirdImage:''
+  },
+  {
+    numeration: '6',
+    name: 'Fucking Phone 7',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    rating: 4.5,
+    link:'',
+    mainImage:'',
+    secondImage:'',
+    thirdImage:''
+  },
+  {
+    numeration: '7',
+    name: 'Fucking Phone SE',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    rating: 4.5,
+    link:'',
+    mainImage:'',
+    secondImage:'',
+    thirdImage:''
+  },
+  {
+    numeration: '8',
+    name: 'Fucking Phone 6S',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    rating: 4.5,
+    link:'',
+    mainImage:'',
+    secondImage:'',
+    thirdImage:''
+  },
+  {
+    numeration: '9',
+    name: 'Fucking Phone 6',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    rating: 4.5,
+    link:'',
+    mainImage:'',
+    secondImage:'',
+    thirdImage:''
+  },
+  {
+    numeration: '10',
+    name: 'Fucking Phone 5S',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    rating: 4.5,
+    link:'',
+    mainImage:'',
+    secondImage:'',
+    thirdImage:''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
